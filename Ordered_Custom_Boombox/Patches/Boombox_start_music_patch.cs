33c25e428fbcc66e100ad1f1f779bc3f3e5a913a
andre <andre@COMPUTATO-V2>
2024-04-20 11:38:03 -0600

    Add project files.

---
+++ Ordered_Custom_Boombox/Patches/Boombox_start_music_patch.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine.Rendering;
+
+namespace Ordered_Custom_Boombox.Patches
+{
+    [HarmonyPatch(typeof(BoomboxItem))]
+    internal class Boombox_start_music_patch
+    {
+
+        private static int track_num = 0;
+        public static int next_track(int unused, int length)
+        {
+            track_num++;
+            if (track_num >= length)
+            {
+                track_num = 0;
+            }
+            return track_num;
+        }
+
+        [HarmonyPatch("StartMusic")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> patch_start_music(IEnumerable<CodeInstruction> instructions)
+        {
+            int a = 0;
+            int b = 1;
+            int c = b + a;
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; ++i)
+            {
+                // todo don't hardcode these indices
+                if (codes[i].opcode == OpCodes.Ldfld && i == 7)
+                {
+                    Ordered_custom_boombox_base.LogInfo(codes[i - 1].ToString());
+                    Ordered_custom_boombox_base.LogInfo(codes[i].ToString());
+                    codes[i - 1] = new CodeInstruction(OpCodes.Nop);
+                    codes[i] = new CodeInstruction(OpCodes.Nop);
+                    Ordered_custom_boombox_base.LogInfo("new: " + codes[i - 1].ToString());
+                    Ordered_custom_boombox_base.LogInfo("new: " + codes[i].ToString());
+                }
+                else if (codes[i].Calls(typeof(Random).GetMethod("Next", new Type[] { typeof(Int32), typeof(Int32) })))
+                {
+                    Ordered_custom_boombox_base.LogInfo(codes[i].ToString());
+                    var classtype = typeof(Boombox_start_music_patch);
+                    var funcname = nameof(Boombox_start_music_patch.next_track);
+                    Ordered_custom_boombox_base.LogInfo(classtype.ToString());
+                    Ordered_custom_boombox_base.LogInfo(funcname.ToString());
+                    codes[i] = new CodeInstruction(OpCodes.Call, AccessTools.Method(classtype, funcname, new Type[] { typeof(Int32), typeof(Int32) }));
+                    Ordered_custom_boombox_base.LogInfo("new: " + codes[i].ToString());
+                }
+            }
+
+            return codes;
+        }
+    }
+}
---
+++ Ordered_Custom_Boombox/Plugin.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+//todo make into not infinite sprint
+
+namespace Ordered_Custom_Boombox
+{
+    [BepInPlugin(mod_GUID, mod_name, mod_version)]
+    public class Ordered_custom_boombox_base : BaseUnityPlugin
+    {
+        private const string mod_GUID = "Hackattack242.Ordered_Custom_Boombox";
+        private const string mod_name = "Ordered Custom Boombox";
+        private const string mod_version = "0.0.1";
+
+        private readonly Harmony harmony = new Harmony(mod_GUID);
+
+        private static Ordered_custom_boombox_base Instance;
+
+        private ManualLogSource logger;
+
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            logger = BepInEx.Logging.Logger.CreateLogSource(mod_name);
+
+            logger.LogInfo("Sample mod has awakened! :D");
+
+            harmony.PatchAll(typeof(Ordered_custom_boombox_base));
+            harmony.PatchAll(typeof(Patches.Boombox_start_music_patch));
+        }
+
+        internal static void LogDebug(string message)
+        {
+            Instance.Log(message, (LogLevel)32);
+        }
+
+        internal static void LogInfo(string message)
+        {
+            Instance.Log(message, (LogLevel)16);
+        }
+
+        internal static void LogWarning(string message)
+        {
+            Instance.Log(message, (LogLevel)4);
+        }
+
+        internal static void LogError(string message)
+        {
+            Instance.Log(message, (LogLevel)2);
+        }
+
+        internal static void LogError(Exception ex)
+        {
+            Instance.Log(ex.Message + "\n" + ex.StackTrace, (LogLevel)2);
+        }
+
+        private void Log(string message, LogLevel logLevel)
+        {
+            ((Ordered_custom_boombox_base)this).logger.Log(logLevel, (object)message);
+        }
+
+}
+}
